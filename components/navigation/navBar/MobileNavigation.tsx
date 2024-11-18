69a2193080e92672926d555ad5d7d768f23393aa
umashankar <=>
2024-11-19 00:22:54 +0530

    Mobile Nav and Nav Links

--- components/navigation/navBar/MobileNavigation.tsx
+++ components/navigation/navBar/MobileNavigation.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
+import ROUTES from "@/constants/routes";
+
+import NavLinks from "./NavLinks";
 
 const MobileNavigation = () => {
   return (
-    <div>MobileNavigation</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="icons/hamburger.svg"
+          alt="Menu"
+          width={36}
+          height={36}
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        className="background-light900_dark200 border-none"
+        side="left"
+      >
+        <SheetTitle className="hidden">Navigation</SheetTitle>
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/images/site-logo.svg"
+            alt="Logo"
+            width={23}
+            height={23}
+          />
+          <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
+            Dev <span className="text-primary-500">Flow</span>
+          </p>
+        </Link>
+        <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+          <SheetClose asChild>
+            <section className="flex h-full flex-col gap-2 py-8">
+              <NavLinks isMobileNav />
+            </section>
+          </SheetClose>
+          <div className="flex flex-col gap-3">
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_IN}>
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Sign In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_UP}>
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNavigation
+export default MobileNavigation;
